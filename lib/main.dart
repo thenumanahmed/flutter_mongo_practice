35c8eefa1dc883c1d40ed61353e132e848c7a3d4
numan ahmed <numanahmedmail@gmail.com>
2023-04-20 12:21:49 +0500

    main.dart code cleaned

--- lib/main.dart
+++ lib/main.dart
@@ -20,35 +20,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const QueryDatabase(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: Text('Hhello'),
+      home: Scaffold(
+        body: const QueryDatabase(),
+        floatingActionButton: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.add),
+        ),
       ),
     );
   }
