d533a78dd72fe410b5c7163a0dd66bfb41110cb1
numan ahmed <numanahmedmail@gmail.com>
2023-04-20 12:21:30 +0500

    display and main.dart code cleaned

--- lib/display.dart
+++ lib/display.dart
@@ -20,13 +20,13 @@ class _MongoDbDisplayState extends State<MongoDbDisplay> {
               future: MongoDatabase.getData(),
               builder: (context, AsyncSnapshot snapshot) {
                 if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(
+                  return const Center(
                     child: CircularProgressIndicator(),
                   );
                 } else {
                   if (snapshot.hasData) {
                     var totalData = snapshot.data.length;
-                    print('debug: total data ' + totalData.toString());
+                    // print('debug: total data $totalData');
                     return Flexible(
                       child: ListView.builder(
                           itemCount: snapshot.data.length,
@@ -36,7 +36,7 @@ class _MongoDbDisplayState extends State<MongoDbDisplay> {
                           }),
                     );
                   } else {
-                    return Center(
+                    return const Center(
                       child: Text('No Data Available'),
                     );
                   }
@@ -54,19 +54,19 @@ class _MongoDbDisplayState extends State<MongoDbDisplay> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('${data.id.$oid}'),
-            SizedBox(
+            Text(data.id.$oid),
+            const SizedBox(
               height: 5,
             ),
-            Text('${data.firstname}'),
-            SizedBox(
+            Text(data.firstname),
+            const SizedBox(
               height: 5,
             ),
-            Text('${data.lastname}'),
-            SizedBox(
+            Text(data.lastname),
+            const SizedBox(
               height: 5,
             ),
-            Text('${data.address}'),
+            Text(data.address),
           ],
         ),
       ),
