1226354c40670d6831ed5e2cb70b84248b49e0a0
Numan Ahmed <97792580+thenumanahmed@users.noreply.github.com>
2023-08-13 12:01:31 +0500

    Update constant.dart

--- lib/dbhelper/constant.dart
+++ lib/dbhelper/constant.dart
@@ -1,3 +1,3 @@
 const MONGO_CONN_URL =
-    "mongodb+srv://user_1:abc@#@#1@cluster0.inlsxzk.mongodb.net/mydatabase?retryWrites=true&w=majority";
+    "mongodb+srv://user_1:abc@#@#01@cluster0.inlsxzk.mongodb.net/mydatabase?retryWrites=true&w=majority";
 const USER_COLLECTION = "youtube";
